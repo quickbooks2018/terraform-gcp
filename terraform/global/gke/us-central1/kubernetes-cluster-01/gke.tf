ad39c41301788b6d76611a4d22e0be1b7fb1a130
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2024-05-26 08:02:49 +0500

    Update gke.tf

--- terraform/global/gke/us-central1/kubernetes-cluster-01/gke.tf
+++ terraform/global/gke/us-central1/kubernetes-cluster-01/gke.tf
@@ -10,6 +10,31 @@ data "terraform_remote_state" "vpc" {
 }
 
 
+# Enable all required api for GKE
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "cloudresourcemanager" {
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "iam" {
+  service = "iam.googleapis.com"
+}
+
+resource "google_project_service" "monitoring" {
+  service = "monitoring.googleapis.com"
+}
+
+resource "google_project_service" "logging" {
+  service = "logging.googleapis.com"
+}
+
 
 data "google_client_config" "default" {}
 
